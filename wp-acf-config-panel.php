53527ba9a5c33e111a8e61dd4d25060362e4ff96
MarJC5 <admin@marjc.ch>
2021-12-24 01:36:19 +0100

    Update wp-acf-config-panel.php

--- wp-acf-config-panel.php
+++ wp-acf-config-panel.php
@@ -3,7 +3,7 @@
  * Plugin URI:        https://github.com/MarJC5/wp-acf-config-panel
  * Description:       Plugin to add a Config panel in Wordpress wp-admin. This will allow us to create a default ACF for static website contents (eg: phone, street, social links etc ...) 
  * Version:           1.0.0
- * Requires at least: 5.2
+ * Requires at least: 5.8.2
  * Requires PHP:      7.4.1
  * Author:            Martin Jean-Christio
  * Author URI:        https://github.com/MarJC5
@@ -11,3 +11,18 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-acf-config-panel
  */
+
+define( 'ABSPATH' ) or die ( 'Unauthorized Access' );
+
+if ( function_exists( 'acf_add_options_page' )) {
+    acf_add_options_page(
+        array(
+            'page_title' => 'Config Panel Page',
+            'menu_title' => 'Config Panel',
+            'redirect'   => false,
+            'position'   => 2,
+            'icon_url'   => 'dashicons-feedback',
+            'capability' => 'administrator'
+        )
+    );
+}
